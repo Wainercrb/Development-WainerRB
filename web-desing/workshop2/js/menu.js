b6862e10be3d36f32f5f3d64b687c34c5290479c
2011.peswainer.1996@gmail.com <61250382>
2017-06-03 00:24:49 -0600

    new code of desing
    I add new code for desing grafic of the page

--- web-desing/workshop2/js/menu.js
+++ web-desing/workshop2/js/menu.js
@@ -1,8 +1,17 @@
 var btnMenu = document.getElementById('btnMenu'); /*menu variable*/
 var nav = document.getElementById('nav'); /*nav variable*/
-
+var numero
 /*code for the transition*/
 
 btnMenu.addEventListener('click', function(){
     nav.classList.toggle('mostrar');
 })
+
+
+function adelante(){
+    numero++;
+    if(numero == 4)
+    numero = 1;
+    var foto = document.getElementById("foto");
+    foto.src= "gallery/foto"+numero+".jpg";M
+}

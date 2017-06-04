75297935f3fb95e795dc5f17963db8bc7b0fa6f6
2011.peswainer.1996@gmail.com <61250382>
2017-06-04 01:25:03 -0600

    I add a galley manuel
    I created a galley wiht two button, Where I can pass the images and vice
    versa

--- web-desing/workshop2/js/menu.js
+++ web-desing/workshop2/js/menu.js
@@ -1,17 +1,8 @@
 var btnMenu = document.getElementById('btnMenu'); /*menu variable*/
 var nav = document.getElementById('nav'); /*nav variable*/
-var numero
 /*code for the transition*/
 
 btnMenu.addEventListener('click', function(){
     nav.classList.toggle('mostrar');
 })
 
-
-function adelante(){
-    numero++;
-    if(numero == 4)
-    numero = 1;
-    var foto = document.getElementById("foto");
-    foto.src= "gallery/foto"+numero+".jpg";M
-}

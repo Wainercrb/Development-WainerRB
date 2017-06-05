0f60a9ec385852ea97165536de75bc9a9f8277d1
2011.peswainer.1996@gmail.com <61250382>
2017-06-04 20:05:52 -0600

    I add more desing
    I add more desing at part of the gallery in the part of estilo.css

--- web-desing/workshop2/js/menu.js
+++ web-desing/workshop2/js/menu.js
@@ -2,7 +2,6 @@ var btnMenu = document.getElementById('btnMenu'); /*menu variable*/
 var nav = document.getElementById('nav'); /*nav variable*/
 /*code for the transition*/
 
-btnMenu.addEventListener('click', function(){
+btnMenu.addEventListener('click', function () {
     nav.classList.toggle('mostrar');
 })
-

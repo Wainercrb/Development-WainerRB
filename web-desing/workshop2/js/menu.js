60db6039d0f106c6e301e1f9fc0f773bce0d6063
2011.peswainer.1996@gmail.com <61250382>
2017-06-07 19:45:30 -0600

    Revert "I move the rama ot"
    This reverts commit 27a6fad543753b396035110629f1471c886bf701.

--- web-desing/workshop2/js/menu.js
+++
@@ -1,7 +0,0 @@
-var btnMenu = document.getElementById('btnMenu'); /*menu variable*/
-var nav = document.getElementById('nav'); /*nav variable*/
-/*code for the transition*/
-
-btnMenu.addEventListener('click', function () {
-    nav.classList.toggle('mostrar');
-})

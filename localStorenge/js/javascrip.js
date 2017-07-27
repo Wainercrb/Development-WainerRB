4f3b10b0b76df57e3cc1aac4c3acf7b9ec3b63e2
2011.peswainer.1996@gmail.com <61250382>
2017-07-26 21:47:06 -0600

    fin
    Se Termina el ejemplo

--- localStorenge/js/javascrip.js
+++ localStorenge/js/javascrip.js
@@ -4,25 +4,23 @@ var btnVer = document.getElementById("btnVer");
 function guardar() {
     var inputNombre = document.getElementById("nombre");
     var inputApellido = document.getElementById("apellido");
+    var inputEmail = document.getElementById("email");
+    var inputTelefono = document.getElementById("telefono");
     localStorage.setItem("nombre", inputNombre.value);
     localStorage.setItem("apellido", inputApellido.value);
+    localStorage.setItem("email", inputEmail.value);
+    localStorage.setItem("telefono", inputTelefono.value);
 }
 
 
-function mensaje() {
-
-    alert("mensaje");
-}
-
-function ver(){
-alert("Los datos guardados son: "+localStorage.getItem("nombre") + " apllido " + localStorage.getItem("apellido"));
+function ver() {
+    alert("Los datos guardados son: " + localStorage.getItem("nombre") + " apllido " + localStorage.getItem("apellido") +"\n" +" email " + localStorage.getItem("email") + " teléfono " + localStorage.getItem("telefono"));
 }
 
 
 btn.onclick = function () {
-   guardar();
+    guardar();
 }
-btnVer.onclick = function (){
+btnVer.onclick = function () {
     ver();
 }
-

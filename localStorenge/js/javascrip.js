7e30b9061648b98f99f371497b88e14196ef3222
2011.peswainer.1996@gmail.com <61250382>
2017-07-26 21:35:06 -0600

    nuevo codigo
    Se agrega las clases js para poder guardar datos de los inputs en local
    storange

--- localStorenge/js/javascrip.js
+++ localStorenge/js/javascrip.js
@@ -1,4 +1,28 @@
-var txtnombre = document.getElementById('nombre');
-var  txtApellido = document.getElementById('nombre');
-var btnGuardar  = document.getElementById('nombre');
+var btn = document.getElementById("button");
+var btnVer = document.getElementById("btnVer");
+
+function guardar() {
+    var inputNombre = document.getElementById("nombre");
+    var inputApellido = document.getElementById("apellido");
+    localStorage.setItem("nombre", inputNombre.value);
+    localStorage.setItem("apellido", inputApellido.value);
+}
+
+
+function mensaje() {
+
+    alert("mensaje");
+}
+
+function ver(){
+alert("Los datos guardados son: "+localStorage.getItem("nombre") + " apllido " + localStorage.getItem("apellido"));
+}
+
+
+btn.onclick = function () {
+   guardar();
+}
+btnVer.onclick = function (){
+    ver();
+}
 

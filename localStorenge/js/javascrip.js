2fd2b44fd335c1a2a786586ff81725b71a142dd7
2011.peswainer.1996@gmail.com <61250382>
2017-07-28 13:36:48 -0600

    nuevo codigo
    Se agregan los metodos para agregar y eliminar el ultimo de la lista y
    guardar y local storage un arreglo de objetos

--- localStorenge/js/javascrip.js
+++ localStorenge/js/javascrip.js
@@ -1,26 +1,78 @@
-var btn = document.getElementById("button");
-var btnVer = document.getElementById("btnVer");
+var listPeople = [];
+document.querySelector('#button').addEventListener('click', capturar);
 
-function guardar() {
-    var inputNombre = document.getElementById("nombre");
-    var inputApellido = document.getElementById("apellido");
-    var inputEmail = document.getElementById("email");
-    var inputTelefono = document.getElementById("telefono");
-    localStorage.setItem("nombre", inputNombre.value);
-    localStorage.setItem("apellido", inputApellido.value);
-    localStorage.setItem("email", inputEmail.value);
-    localStorage.setItem("telefono", inputTelefono.value);
+document.querySelector('#btnVer').addEventListener('click', verLocalStorange);
+document.querySelector('#btnDelete').addEventListener('click', eliminarUltimo);
+
+
+
+function capturar() {
+    var people = document.querySelector('#nombre').value,
+        apellido = document.querySelector('#apellido').value,
+        email = document.querySelector('#email').value,
+        telefono = document.querySelector('#telefono').value;
+
+    addPeople(people, apellido, email, telefono);
+
+
+}
+
+function addPeople(pNombre, pApellido, pEmil, pTelefono) {
+
+    var newPeople = {
+        nombre: pNombre,
+        apellido: pApellido,
+        email: pEmil,
+        telefono: pTelefono
+    };
+    console.log(newPeople);
+    listPeople.push(newPeople);
+    guardarLista(listPeople);
+}
+
+function guardarLista(peopleList) {
+    localStorage.setItem('lisOfPeople', JSON.stringify(peopleList));
+}
+
+function getList() {
+    var temList = localStorage.getItem('lisOfPeople');
+    if (temList != null) {
+
+        listPeople = JSON.parse(temList);
+    } else {
+        listPeople = [];
+        alert("No hay registros en el local storange");
+    }
+    alert(temList.length);
+    if (temList.length === 2){
+        alert("No hay registros en el local storange");
+    }
+    return listPeople;
+}
+
+function verLocalStorange() {
+
+    var mensaje = "";
+    getList();
+    for (i = 0; i < listPeople.length; i++) {
+        mensaje += "Nombre  " + listPeople[i].nombre + " Apellido " + listPeople[i].apellido + "Teléfono " + listPeople[i].telefono + "Email " +
+            listPeople[i].email + "\n" + "-----------------------------------" + "\n";
+    }
+    comment.value = mensaje;
 }
 
 
-function ver() {
-    alert("Los datos guardados son: " + localStorage.getItem("nombre") + " apllido " + localStorage.getItem("apellido") +"\n" +" email " + localStorage.getItem("email") + " teléfono " + localStorage.getItem("telefono"));
-}
+function eliminarUltimo() {
 
+    getList();
 
-btn.onclick = function () {
-    guardar();
-}
-btnVer.onclick = function () {
-    ver();
+    if (listPeople <= 0) {
+        alert("No hay datos para eliminar");
+    } else {
+        var lcStorange = JSON.parse(localStorage.getItem('lisOfPeople'));    
+        lcStorange.splice(lcStorange.length-1);
+        localStorage.setItem('lisOfPeople', JSON.stringify(lcStorange));
+        alert("Se elimino correctamente");
+        verLocalStorange();
+    }
 }
